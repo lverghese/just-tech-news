9dd1f7effe46c7768a0249db86e18cc149733da2
lverghese <loganverghese@gmail.com>
2021-09-15 15:30:15 -0400

    not functioning properly

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,9 @@
 const router = require('express').Router();
 
-const { User } = require('../models');
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
 
 module.exports = router;
